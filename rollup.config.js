4662dba78af1ac0b2c047cea2660fabae0f93457
Noor Imran <noor.imran@itb.local>
2024-03-04 20:27:04 +0500

    --Update: configuration files to publish on npm--

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typscript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss"
+
 
 const packageJson = require("./package.json");
 
@@ -24,11 +26,13 @@ export default [
             resolve(),
             commonjs(),
             typscript({tsconfig: "./tsconfig.json"}),
+            postcss()
         ]
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{file: "dist/index.d.ts", format: "esm"}],
         plugins: [dts()],
+        external: [/\.css$/],
     }
 ]
